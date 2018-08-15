d64b513979ee73979172674fcc7ed121f1a08fdb
elizzzabethrae <elizabethwarners@gmail.com>
2018-08-14 20:52:36 -0400

    fixed router checkpoitn

--- src/App.js
+++ src/App.js
@@ -16,8 +16,8 @@ class App extends Component {
           <h1> Bloc Jams </h1>
         </header>
         <main>
-          <Route exact path="/" component={Landing} />
-          <Route path="/library" component={Library} />
+          <Route exact path="/" component = {Landing} />
+          <Route path="/library" component = {Library} />
         </main>
       </div>
     );
