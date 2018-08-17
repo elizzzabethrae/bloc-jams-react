2ed6eb278795f4be0d5353d2c8e588210b9bfbc7
elizzzabethrae <elizabethwarners@gmail.com>
2018-08-16 20:15:08 -0400

    checkpoint work for library component

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,1 +1,14 @@
-// something here
+
+import React, { Component } from 'react';
+
+class Album extends Component {
+  render() {
+    return (
+      <section className="album">
+         {this.props.match.params.slug} Album will go here
+      </section>
+    );
+  }
+}
+
+export default Album;
