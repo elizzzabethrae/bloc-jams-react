9e5030a8104887dea2e5ae4d277cc287aa2dcdd6
elizzzabethrae <elizabethwarners@gmail.com>
2018-08-31 15:55:37 -0400

    styling finished

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import './App.css';
 import Landing from './components/Landing';
 import Library from './components/Library';
 import Album from './components/Album';
+import logo from './bloc_jams_logo.png';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header>
-          <img src ='bloc_jams_logo.png' alt = "bloc jams logo"/>
+          <img className = "logo" src={logo} alt = "bloc jams logo" align='left'/>
           <nav>
-            <Link to='/'>Landing</Link>
-            <Link to='/library'>Library</Link>
+          <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/library'>Library</Link></li>
+          </ul>
           </nav>
-          <h1> Bloc Jams</h1>
+        {/*  <h1> Bloc Jams</h1>*/}
         </header>
         <main>
           <Route exact path="/" component = {Landing} />
